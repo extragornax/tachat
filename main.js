4caf4456ea490d83f610c2921dbda7ab54358154
Jamason Davis <jpdavis2471@gmail.com>
2018-05-10 11:42:06 -0500

    new dir setup

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const url = require('url')
 function createWindow () {
     win = new BrowserWindow({width: 1280, height: 720})
     win.loadURL(url.format ({
-        pathname: path.join(__dirname, 'index.html'),
+        pathname: path.join('resources', 'index.html'),
         protocol: 'file:',
         slashes: true
     }))
